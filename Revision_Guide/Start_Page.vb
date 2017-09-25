6998c3ff018304e1107d6238f1d66936fc606696
Tomiwa Oyebamiji <TomOye161@student.loreto.ac.uk>
2017-09-25 09:59:04 +0100

    Add project files.

---
+++ Revision_Guide/Start_Page.vb
@@ -0,0 +1,11 @@
+﻿Public Class Start_Page
+    Private Sub Start_Page_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Forms.Menu_Page.Text = Now.ToString
+        My.Forms.Menu_Page.Show()
+        Me.Hide()
+    End Sub
+End Class
